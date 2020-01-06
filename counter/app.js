e7029a8d2df589e083da11f9a488f2648f0a7d05
daankh <jarosz.dan@gmail.com>
2020-01-06 07:30:29 +0100

    add counter module

---
+++ counter/app.js
@@ -0,0 +1,6 @@
+const counter = require("./counter");
+
+counter.add(3);
+counter.add(30);
+counter.sup(10);
+counter.sup(-7);
---
+++ counter/counter.js
@@ -0,0 +1,13 @@
+let counter = 0;
+
+// module.exports = () => console.log(++counter);
+
+module.exports.add = number => {
+  counter += number;
+  console.log(counter);
+};
+
+module.exports.sup = number => {
+  counter -= number;
+  console.log(counter);
+};
