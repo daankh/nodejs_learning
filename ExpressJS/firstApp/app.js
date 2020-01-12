e5be85bfe61e2103764c0214230bf8d5f893c188
daankh <jarosz.dan@gmail.com>
2020-01-12 06:53:06 +0100

    add Express.js examples

--- ExpressJS/firstApp/app.js
+++ ExpressJS/firstApp/app.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, () => {
   console.log(`Server is listening at localhost:${port}`);
 });
 
-app.get("/", () => {
-  console.log("Hello wordl!");
-});
+// app.get("/", req => {
+//   console.log("Hello world");
+//   // console.log(req.hostname);
+//   // console.log(req.ip);
+//   // console.log(req.ips);
+//   // console.log(req.method);
+//   // console.log(req.url);
+//   // console.log(req.orginalUrl);
+//   // console.log(req.path);
+// });
 
 app.get("/hi", () => {
   console.log("Hi wordl!");
+});
+
+app.all("/", req => {
+  // Metody request
+  // console.log("Hello world");
+  console.log(req.hostname);
+  console.log(req.ip);
+  console.log(req.ips);
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.orginalUrl);
+  console.log(req.path);
+  console.log(req.protocol);
+  console.log(req.secure);
 });
